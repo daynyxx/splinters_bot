e0783fca6b964fa68884562eb3632a996ac6afec
daynyxx <68802351+daynyxx@users.noreply.github.com>
2020-07-31 16:47:15 -0400

    Update env.ex

--- lib/helpers/env.ex
+++ lib/helpers/env.ex
@@ -25,10 +25,6 @@ defmodule SplintersBotElixir.Env do
       :youtube -> System.get_env("YOUTUBE")
       :insta -> System.get_env("INSTA")
       :twitter -> System.get_env("TWITTER")
-      :clientid -> System.get_env("CLIENTID")
-      :secret -> System.get_env("CLIENTSECRET")
-      :oauth -> System.get_env("OAUTH")
-      :refresh -> System.get_env("REFRESH")
       _ -> ""
     end
   end

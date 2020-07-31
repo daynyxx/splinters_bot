2b7f5af9db34faac37c9c8805cf9cef6031a41ea
daynyxx <68802351+daynyxx@users.noreply.github.com>
2020-07-31 16:49:48 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule SplintersBotElixir.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      applications: [:hedwig_irc_adapter, :httpoison],
+      applications: [:hedwig_irc_adapter],
       extra_applications: [:logger],
       mod: {SplintersBotElixir.Application, []}
     ]
@@ -23,9 +23,7 @@ defmodule SplintersBotElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:hedwig_irc_adapter, "~> 0.1.0"},
-      {:json, "~> 1.3.0"},
-      {:httpoison, "~> 1.7.0"}
+      {:hedwig_irc_adapter, "~> 0.1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
